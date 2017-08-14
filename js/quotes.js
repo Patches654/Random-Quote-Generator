38e2ff6263847369e9d560ecb6aceba725a2e979
Benjamin Houlihan <benjamin.houlihan@outlook.com>
2017-08-15 02:12:13 +1000

    Quotes
    Quotes are now comming up on the web page!

---
+++ js/quotes.js
@@ -0,0 +1,33 @@
+// Array & Objects
+quotes = [
+  {
+    quote: "We all go a little mad sometimes",
+    source: "Anthony Perkins",
+    citation: "Pshyco",
+    year: 1960
+  },
+  {
+    quote: "Shaken - Not stirred",
+    source: "Sean Connery",
+    citation: "James Bond",
+    year: 1964
+  },
+  {
+    quote: "Toto, I've got a feeling we're not in Kansas anymore",
+    source: "Judy Garland",
+    citation: "The Wizard of Oz",
+    year: 1939
+  },
+  {
+    quote: "Wax On, Wax Off",
+    source: "",
+    citation: "The Karate Kid",
+    year: 1984
+  },
+  {
+    quote: "What we think, we become",
+    source:"Buddah",
+    citation: "",
+    year: 0
+  }
+];
--- js/script.js
+++ js/script.js
@@ -7,41 +7,6 @@ var randomQuote
 var Num
 var quoteList
 
-// Array & Objects
-quotes = [
-  {
-    quote: "We all go a little mad sometimes",
-    source: "Anthony Perkins",
-    citation: "Pshyco",
-    year: 1960
-  },
-  {
-    quote: "Shaken - Not stirred",
-    source: "Sean Connery",
-    citation: "James Bond",
-    year: 1964
-  },
-  {
-    quote: "Toto, I've got a feeling we're not in Kansas anymore",
-    source: "Judy Garland",
-    citation: "The Wizard of Oz",
-    year: 1939
-  },
-  {
-    quote: "Wax On, Wax Off",
-    source: "",
-    citation: "The Karate Kid",
-    year: 1984
-  },
-  {
-    quote: "What we think, we become",
-    source:"Buddah",
-    citation: "",
-    year: 0
-  }
-];
-
-
 // Functions
 function getRandomQuote(randomQuote){
   var randomNumber = getRandomNumber(Num);
@@ -54,10 +19,15 @@ function getRandomNumber(Num){
   return randomNumber;
 }
 
-function printQuote(quoteList){
+function printQuote(){
   var quoteObj = getRandomQuote(randomQuote);
-  return quoteObj;
-}
+  console.log(quoteObj);
 
-// Entry Point
-printQuote(quoteList);
+  var quoteString = '<p class="quote">' + quoteObj.quote + '</p>';
+  quoteString += '<p class="source">' + quoteObj.source;
+  quoteString +=  '<span class="citation">' + quoteObj.citation + '</span>';
+  quoteString +=  '<span class="year">' + quoteObj.year + '</span></p>'
+
+document.getElementById("quote-box").innerHTML = quoteString;
+  return;
+}
